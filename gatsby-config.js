51eda783ac1250eaa2b9eb7a8b715e1a8fa8c3fb
marco-studio <60696119+marco-studio@users.noreply.github.com>
2020-02-11 10:32:11 +0000

    finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: `Over The Bridge`,
-    description: `Hey, meet Bilal, Kwaku, Patrick &amp; Tom. 4 Black &amp; Mixed-Race guys who became friends whilst studying at Cambridge University. Join us as we talk about life before, during and well - after 'The Bridge'. Expect chats about life, and our own experiences.
-    email: otbpodcastuk@gmail.com
-    Twitter: @otbpodcastuk
-    Cast: 
-    Kwaku: @KwakuDapaah_
-    Patrick: @CariocoLondrino
-    Bilal: @Tweetsbybilal
-    Tom: @TomTheEconomist`,
-    author: `Kwaku, Patrick, Bilal & Tom`,
-    twitter: `https://twitter.com/otbpodcastuk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor`,
-    instagram: `https://www.instagram.com/otbpodcastuk/`,
-    facebook: ``,
+    title: `In Out Finland podcast`,
+    description: `Just some dudes and one sassy queen talking about different stuff IN & OUT of  FINLAND! 
+    In reality we just rumble, stutter and are super awkward...`,
+    author: `inoutfinland`,
+    twitter: ``,
+    instagram: `https://www.instagram.com/inoutfinland/`,
+    facebook: `https://www.facebook.com/inoutfinland`,
     youtube: ``,
-    apple: `https://podcasts.apple.com/podcast/id1364395842?ct=podlink&mt=2`,
+    apple: `https://podcasts.apple.com/podcast/id1497187925?ct=podlink&mt=2`,
     stitcher: ``,
-    google: `https://podcasts.google.com/?feed=aHR0cDovL2ZlZWRzLnNvdW5kY2xvdWQuY29tL3VzZXJzL3NvdW5kY2xvdWQ6dXNlcnM6NDA3NTYwNzY0L3NvdW5kcy5yc3M=`,
-    pocket: `https://pca.st/itunes/1364395842`,
-    spotify: `https://open.spotify.com/show/0Lo2a3hwV0nxymHUj2lNIk`,
-    overcast: `https://overcast.fm/itunes1364395842`,
-    castbox: `https://soundcloud.com/overthebridgeuk`,
-    castro: `https://castro.fm/itunes/1364395842`,
+    google: `https://podcasts.google.com/?feed=aHR0cHM6Ly9pbm91dGZpbmxhbmRwb2RjYXN0LmxpYnN5bi5jb20vcnNz`,
+    pocket: `https://pca.st/itunes/1497187925`,
+    spotify: `https://open.spotify.com/show/7G558PtZW9HBrQmtLpN7pv`,
+    overcast: `https://overcast.fm/itunes1497187925`,
+    castbox: `https://castbox.fm/vic/1497187925`,
+    castro: `https://castro.fm/itunes/1497187925`,
     podbean: `https://www.podbean.com/podcast-detail/g9v5c-68ac3/Over-The-Bridge-Podcast`,
-    beaker: `https://www.breaker.audio/shows?feed_url=http%3A%2F%2Ffeeds.soundcloud.com%2Fusers%2Fsoundcloud%3Ausers%3A407560764%2Fsounds.rss`
+    beaker: `https://www.breaker.audio/shows?feed_url=https%3A%2F%2Finoutfinlandpodcast.libsyn.com%2Frss`,
+    player: `https://player.fm/series/https%253A%252F%252Finoutfinlandpodcast%252Elibsyn%252Ecom%252Frss`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -59,7 +54,7 @@ module.exports = {
         // url: `https://www.gatsbyjs.org/blog/rss.xml`,
         // url: `https://boysinthecave.com/feed/podcast/`,
         // url: `http://boysinthecave.libsyn.com/rss`,
-        url: `http://feeds.soundcloud.com/users/soundcloud:users:407560764/sounds.rss`,
+        url: `http://inoutfinlandpodcast.libsyn.com/rss`,
         name: `GatsbyBlog`,
         // Optional
         // Read parser document: https://github.com/bobby-brennan/rss-parser#readme
